abfd267963996a840c4bf17600510b5743440995
Thomas Botelho <thomasjbotelho@gmail.com>
2022-05-31 20:10:43 -0300

    annealing improvements and code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # IA-G1-2022.1
+
 Trabalho de IA
+
+Para executar o procedimento de simulated annealing use o comando
+
+```
+python annealing.py
+```
+
+É preciso ter a biblioteca NumPy instalada para tal.
+
+O programa executa o procedimento 20 vezes e extrai o melhor resultado.
+O melhor resultado obtido até o momento foi
+
+```
+Tempo total apos annealing: 1822.1074358062847
+[[0 0 0 0 0 0 1]
+ [0 0 0 0 0 1 0]
+ [0 0 0 0 0 1 0]
+ [0 0 0 0 1 0 0]
+ [0 0 0 0 1 0 0]
+ [0 1 0 0 0 0 0]
+ [0 0 1 0 0 0 0]
+ [1 0 0 0 0 0 0]
+ [1 0 0 0 0 0 0]
+ [0 0 0 0 1 0 1]
+ [0 0 1 0 0 0 1]
+ [0 1 0 0 0 0 1]
+ [0 0 0 1 0 0 1]
+ [0 1 0 0 0 1 0]
+ [0 0 1 0 0 1 0]
+ [0 1 0 0 0 1 0]
+ [1 0 0 0 0 1 0]
+ [1 0 0 0 0 1 0]
+ [0 0 1 0 1 0 0]
+ [0 1 0 0 1 0 0]
+ [0 0 0 1 1 0 0]
+ [0 0 0 1 1 0 0]
+ [0 1 0 0 0 1 1]
+ [0 1 0 1 0 0 0]
+ [0 0 1 1 0 0 0]
+ [0 0 1 1 0 0 0]
+ [1 1 0 0 0 0 0]
+ [1 0 0 1 0 0 0]
+ [1 0 0 1 0 0 0]
+ [1 0 1 0 0 0 0]
+ [0 0 1 0 1 0 1]]
+```
+
+Note que cada posição no vetor corresponde a um personagem.
+
+```
+1: Aang
+2: Zukko
+3: Toph
+4: Katara
+5: Sokka
+6: Appa
+7: Momo
+```
--- annealing.py
+++ annealing.py
@@ -7,7 +7,7 @@ ETAPAS = 31
 PERSONAGENS = 7
 
 # associacao entre etapas e suas dificuldades
-# o indice na lista corresponde ao numero da etapa
+# o indice no dict corresponde ao numero da etapa
 dificuldades = []
 for i in range(1, ETAPAS + 1):
     dificuldades.append(10 * i)
@@ -21,17 +21,18 @@ personagens = {
     5: ("appa", 0.9),
     6: ("momo", 0.7),
 }
-# print(dificuldades)
-print("Len:", len(dificuldades))
 
-
+# imprimir solucao formatada
 def print_sol(sol):
     v = sol.view()
     v.shape = (ETAPAS, PERSONAGENS)
     print(v)
 
 
-# garante que na solucao cada personagem tem ate 8 vidas
+# numa solucao valida:
+# - cada personagem tem ate 8 vidas
+# - toda etapa tem pelo menos 1 personagem
+# - pelo menos um personagem sobrevive no final (tem mais de 0 vidas)
 def valida_sol(sol, debug=False):
     v = sol.view()
     v.shape = (ETAPAS, PERSONAGENS)
@@ -44,200 +45,209 @@ def valida_sol(sol, debug=False):
             return False
         idx += 1
 
+    total_vidas = 0
     for i in range(PERSONAGENS):
         vidas = v[:, i].sum()
         p_ind = [*personagens][i]
         personagem = personagens[p_ind][0]
-        # print(f"{personagem}: {vidas}")
+        total_vidas += vidas
+
         if vidas > VIDAS or vidas < 0:
             if debug:
                 print(f"SOLUCAO INVALIDA, {personagem} TEM {vidas}!!")
             return False
 
-    return True
+    return total_vidas < (PERSONAGENS * VIDAS)
 
 
-#
-# annealing funcs
-#
+class SimAnnealing:
+    def total_cost(self, sol):
+        # avalia o tempo levado de acordo com uma solucao
+        tempo = 0
+        v = sol.view()
+        v.shape = (ETAPAS, PERSONAGENS)
+        idx = 0
+        for row in v:
+            agilidade_etapa = 0
+            for i in range(PERSONAGENS):
+                if row[i] == 1:
+                    p_ind = [*personagens][i]
+                    agilidade_etapa += personagens[p_ind][1]
 
-# avalia o tempo levado de acordo com uma solucao
-def total_cost(sol):
-    tempo = 0
-    v = sol.view()
-    v.shape = (ETAPAS, PERSONAGENS)
-    idx = 0
-    for row in v:
-        agilidade_etapa = 0
-        for i in range(PERSONAGENS):
-            # print(idx)
-            if row[i] == 1:
-                p_ind = [*personagens][i]
-                agilidade_etapa += personagens[p_ind][1]
+            if agilidade_etapa == 0:
+                agilidade_etapa = 1e-12
 
-        if agilidade_etapa == 0:
-            agilidade_etapa = 1e-12
+            tempo += dificuldades[idx] / agilidade_etapa
+            idx += 1
 
-        tempo += dificuldades[idx] / agilidade_etapa
-        idx += 1
+        return tempo
 
-    return tempo
-
-
-# troca a posicao de dois bits correspondentes em diferentes etapas
-def swap_bit_op(sol):
-    personagem = random.randrange(0, PERSONAGENS)
-    rands = random.sample(range(0, ETAPAS), 2)
-    e1, e2 = rands[0], rands[1]
-    n_idx1 = PERSONAGENS * e1 + personagem
-    n_idx2 = PERSONAGENS * e2 + personagem
-    sol[[n_idx1, n_idx2]] = sol[[n_idx2, n_idx1]]
-
-
-def swap_bit_op_n(sol, n):
-    personagens = random.sample(range(0, PERSONAGENS), n)
-    rands = random.sample(range(0, ETAPAS), 2)
-    e1, e2 = rands[0], rands[1]
-
-    for p in personagens:
-        n_idx1 = PERSONAGENS * e1 + p
-        n_idx2 = PERSONAGENS * e2 + p
+    def swap_bit_op(self, sol):
+        # troca a posicao de dois bits correspondentes em diferentes etapas
+        personagem = random.randrange(0, PERSONAGENS)
+        rands = random.sample(range(0, ETAPAS), 2)
+        e1, e2 = rands[0], rands[1]
+        n_idx1 = PERSONAGENS * e1 + personagem
+        n_idx2 = PERSONAGENS * e2 + personagem
         sol[[n_idx1, n_idx2]] = sol[[n_idx2, n_idx1]]
 
+    def swap_bit_op_n(self, sol, n):
+        # troca a posicao de n bits correspondentes em diferentes etapas
+        # n deve ser menor que $PERSONAGENS
+        personagens = random.sample(range(0, PERSONAGENS), n)
+        rands = random.sample(range(0, ETAPAS), 2)
+        e1, e2 = rands[0], rands[1]
 
-# troca as solucoes de duas etapas
-def swap_stage_op(sol):
-    rands = random.sample(range(0, ETAPAS), 2)
-    i = rands[0]
-    j = rands[1]
+        for p in personagens:
+            n_idx1 = PERSONAGENS * e1 + p
+            n_idx2 = PERSONAGENS * e2 + p
+            sol[[n_idx1, n_idx2]] = sol[[n_idx2, n_idx1]]
+
+    def swap_stage_op(self, sol):
+        # troca as solucoes de duas etapas
+        rands = random.sample(range(0, ETAPAS), 2)
+        i = rands[0]
+        j = rands[1]
+        v = sol.view()
+        v.shape = (ETAPAS, PERSONAGENS)
+        v[[i, j]] = v[[j, i]]
+
+    def rand_neighbor(self, curr_sol):
+        # aplica os operadores para solucoes "vizinhas"
+        # e usa o que corresponde ao melhor resultado
+        # OBS: os operadores utilizados geram solucoes validas
+        stage_sol = np.copy(curr_sol)
+        self.swap_stage_op(stage_sol)
+
+        bit_sol = np.copy(curr_sol)
+        self.swap_bit_op(bit_sol)
+        bit_2_sol = np.copy(curr_sol)
+        self.swap_bit_op_n(bit_2_sol, 2)
+        bit_3_sol = np.copy(curr_sol)
+        self.swap_bit_op_n(bit_3_sol, 3)
+        bit_4_sol = np.copy(curr_sol)
+        self.swap_bit_op_n(bit_4_sol, 4)
+        bit_5_sol = np.copy(curr_sol)
+        self.swap_bit_op_n(bit_5_sol, 5)
+        bit_6_sol = np.copy(curr_sol)
+        self.swap_bit_op_n(bit_6_sol, 6)
+
+        stage_cost = self.total_cost(stage_sol)
+        bit_cost = self.total_cost(bit_sol)
+        bit_2_cost = self.total_cost(bit_2_sol)
+        bit_3_cost = self.total_cost(bit_3_sol)
+        bit_4_cost = self.total_cost(bit_4_sol)
+        bit_5_cost = self.total_cost(bit_5_sol)
+        bit_6_cost = self.total_cost(bit_6_sol)
+
+        sols = [
+            (stage_cost, stage_sol),
+            (bit_cost, bit_sol),
+            (bit_2_cost, bit_2_sol),
+            (bit_3_cost, bit_3_sol),
+            (bit_4_cost, bit_4_sol),
+            (bit_5_cost, bit_5_sol),
+            (bit_6_cost, bit_6_sol),
+        ]
+
+        for idx, s in enumerate(sols):
+            if not valida_sol(s[1]):
+                sols.pop(idx)
+
+        chosen = min(sols, key=lambda t: t[0])
+        return chosen[1], chosen[0]
+
+    def acceptance_prob(self, cost, n_cost, temp):
+        if n_cost < cost:
+            return 1
+        else:
+            # metropolis criterion
+            p = np.exp(-(n_cost - cost) / temp)
+            return p
+
+    def temperature(self, start_temp, curr_temp, alpha, iter):
+        # linear multiplicative cooling
+        return start_temp / (1 + alpha * iter)
+
+    def solve(self, start_state, start_temp, alpha, maxiter=10000):
+        curr_state = start_state
+        curr_cost = self.total_cost(curr_state)
+        curr_temp = start_temp
+        best = curr_state
+
+        for iter in range(maxiter):
+
+            curr_temp = self.temperature(start_temp, curr_temp, alpha, iter)
+            neighbor_state, neighbor_cost = self.rand_neighbor(curr_state)
+            if curr_cost > neighbor_cost:
+                best = neighbor_state
+
+            if (
+                self.acceptance_prob(curr_cost, neighbor_cost, curr_temp)
+                > np.random.rand()
+            ):
+                curr_state = neighbor_state
+                curr_cost = neighbor_cost
+
+        # print("Ended at temp:", curr_temp)
+        return best, self.total_cost(best)
+
+
+def initial_solution():
+    # Gera uma solucao inicial válida
+    # para tal iteramos sobre as etapas aleatoriamente
+    # acrescentando personagens até que acabem suas vidas.
+    # Estamos assumindo que todos os personagens serão usados 8 vezes,
+    # com excessão do Momo, que tem a menor agilidade
+
+    # inicializacao
+    width = PERSONAGENS * len(dificuldades)
+    sol = np.zeros(width, dtype=int)
+
+    # montar solucao inicial
     v = sol.view()
     v.shape = (ETAPAS, PERSONAGENS)
-    v[[i, j]] = v[[j, i]]
+    fill = [0, 1, 2, 3, 4, 5, 6] * VIDAS
+    fill.remove(6)
+    # random.shuffle(fill)
+    row = 0
+    while len(fill) > 0:
+        fill_idx = fill[-1]
+        if v[row, fill_idx] == 0:
+            v[row, fill_idx] = 1
+            fill.pop()
+
+        row += 1
+        if row >= ETAPAS:
+            row = 0
+
+    valida_sol(sol, debug=True)
+    return sol
 
 
-# gets best neighbor state
-def rand_neighbor(curr_sol):
-    stage_sol = np.copy(curr_sol)
-    swap_stage_op(stage_sol)
-    stage_2_sol = np.copy(curr_sol)
-    swap_stage_op(stage_2_sol)
-    swap_stage_op(stage_2_sol)
-    stage_3_sol = np.copy(curr_sol)
-    swap_stage_op(stage_3_sol)
-    swap_stage_op(stage_3_sol)
-    swap_stage_op(stage_3_sol)
-    # stage_4_sol = np.copy(curr_sol)
-    # swap_stage_op(stage_4_sol)
-    # swap_stage_op(stage_4_sol)
-    # swap_stage_op(stage_4_sol)
-    # swap_stage_op(stage_4_sol)
+def init():
+    # Funcao de conveniencia para inicializar o processo de annealing
+    sol = initial_solution()
+    solver = SimAnnealing()
+    tempo_inicial = solver.total_cost(sol)
+    print("Tempo total inicial:", tempo_inicial)
 
-    bit_sol = np.copy(curr_sol)
-    swap_bit_op(bit_sol)
-    bit_2_sol = np.copy(curr_sol)
-    swap_bit_op_n(bit_2_sol, 2)
-    bit_3_sol = np.copy(curr_sol)
-    swap_bit_op_n(bit_3_sol, 3)
-    bit_4_sol = np.copy(curr_sol)
-    swap_bit_op_n(bit_4_sol, 4)
-    bit_5_sol = np.copy(curr_sol)
-    swap_bit_op_n(bit_5_sol, 5)
-    bit_6_sol = np.copy(curr_sol)
-    swap_bit_op_n(bit_6_sol, 6)
+    best_sol = []
+    best_cost = np.inf
+    for i in range(10):
+        new_sol, new_cost = solver.solve(
+            start_state=sol, start_temp=18.0, alpha=8, maxiter=10000
+        )
+        if new_cost < best_cost:
+            best_cost = new_cost
+            best_sol = new_sol
 
-    stage_cost = total_cost(stage_sol)
-    stage_2_cost = total_cost(stage_2_sol)
-    stage_3_cost = total_cost(stage_3_sol)
-    # stage_4_cost = total_cost(stage_4_sol)
-    bit_cost = total_cost(bit_sol)
-    bit_2_cost = total_cost(bit_2_sol)
-    bit_3_cost = total_cost(bit_3_sol)
-    bit_4_cost = total_cost(bit_4_sol)
-    bit_5_cost = total_cost(bit_5_sol)
-    bit_6_cost = total_cost(bit_6_sol)
-
-    sols = [
-        (stage_cost, stage_sol),
-        (stage_2_cost, stage_2_sol),
-        (stage_3_cost, stage_3_sol),
-        # (stage_4_cost, stage_4_sol),
-        (bit_cost, bit_sol),
-        (bit_2_cost, bit_2_sol),
-        (bit_3_cost, bit_3_sol),
-        (bit_4_cost, bit_4_sol),
-        (bit_5_cost, bit_5_sol),
-        (bit_6_cost, bit_6_sol),
-    ]
-
-    for idx, s in enumerate(sols):
-        if not valida_sol(s[1]):
-            sols.pop(idx)
-
-    chosen = min(sols, key=lambda t: t[0])
-    return chosen[1], chosen[0]
-
-
-def acceptance_prob(cost, n_cost, temp):
-    if n_cost <= cost:
-        return 1
+    if valida_sol(best_sol, debug=True):
+        print("Tempo total apos annealing:", best_cost)
+        print_sol(new_sol)
     else:
-        # metropolis criterion
-        p = np.exp(-(n_cost - cost) / temp)
-        return p
+        print("Solucao encontrada é inválida!")
 
 
-# logarithmic cooling
-def temperature(start_temp, curr_temp, alpha, iter):
-    if curr_temp < 0.000025:
-        return 0.000025
-    return start_temp / (1 + alpha * iter * iter)
-    # return curr_temp * alpha
-
-
-def solve(start_state, start_temp, alpha, maxiter=10000):
-    curr_state = start_state
-    curr_cost = total_cost(curr_state)
-    curr_temp = start_temp
-
-    for iter in range(maxiter):
-        # curr_temp = temperature(start_temp, curr_temp, alpha, float(iter))
-        curr_temp = temperature(start_temp, curr_temp, alpha, iter)
-        neighbor_state, neighbor_cost = rand_neighbor(curr_state)
-        if acceptance_prob(curr_cost, neighbor_cost, curr_temp) > np.random.rand():
-            curr_state = neighbor_state
-            curr_cost = neighbor_cost
-
-    return curr_state, total_cost(curr_state)
-
-
-# largura do vetor de solucoes
-# 7 bits para cada etapa
-width = PERSONAGENS * len(dificuldades)
-sol = np.zeros(width, dtype=int)
-
-# montar solucao inicial
-v = sol.view()
-v.shape = (ETAPAS, PERSONAGENS)
-fill = [0, 1, 2, 3, 4, 5, 6] * VIDAS
-# random.shuffle(fill)
-row = 0
-while len(fill) > 0:
-    fill_idx = fill[-1]
-    if v[row, fill_idx] == 0:
-        v[row, fill_idx] = 1
-        fill.pop()
-
-    row += 1
-    if row >= ETAPAS:
-        row = 0
-
-valida_sol(sol, debug=True)
-tempo = total_cost(sol)
-print("Tempo total inicial:", tempo)
-
-new_sol, new_cost = solve(start_state=sol, start_temp=5000.0, alpha=0.98, maxiter=3000)
-if valida_sol(new_sol):
-    print("Tempo total apos annealing:", new_cost)
-    # print(new_sol)
-else:
-    print("Solucao invalida!")
+if __name__ == "__main__":
+    init()
